db629940e5ee001209b1c3ca5095cb3651196dcf
deenmd786 <153393835+deenmd786@users.noreply.github.com>
2023-12-14 20:08:47 +0000

    maths and num

---
+++ 01_basics/01_Varibles.js
@@ -0,0 +1,12 @@
+const accountId = 4578864
+let accountEmail = "deen@gmail.com"
+var accountPassword = 454548
+accountCity = "Delhi"
+// accountId = 25
+accountEmail = "211554"
+accountPassword = 10000
+accountCity = "up"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
---
+++ 01_basics/02_conversionOpration.js
@@ -0,0 +1,17 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueToNumber = Number(score);
+
+console.log(typeof valueToNumber);
+console.log(typeof (valueToNumber));
+
+// let valueToString = String(valueToNumber);
+let valueToString = Boolean(valueToNumber);
+// let valueToString = Object(valueToNumber);
+// let valueToString = Object(valueToNumber);
+
+console.log(typeof valueToString);
+console.log( valueToString);
